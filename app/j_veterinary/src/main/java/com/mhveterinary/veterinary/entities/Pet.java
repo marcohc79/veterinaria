72d7d237d3d7a85f6dff039a04298a27554cc6b9
Marco Huamani <mhcaballero79@gmail.com>
2024-12-29 23:56:44 -0300

    I created Class Pet

--- app/j_veterinary/src/main/java/com/mhveterinary/veterinary/entities/Pet.java
+++ app/j_veterinary/src/main/java/com/mhveterinary/veterinary/entities/Pet.java
@@ -1,18 +1,41 @@
 package com.mhveterinary.veterinary.entities;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDate;
 
 @Entity
+@Data
+@Table(name = "PETS")
 public class Pet {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Integer id;
-    String name;
-    String breed;
-    String type;
-    String color;
-    LocalDate birthday;
-    String microchip_id;
+    private Integer id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "breed", length = 50)
+    private String breed;
+
+    @Column(name = "type", nullable = false, length = 50)
+    private String type;
+
+    @Column(name = "colour", nullable = false, length = 50)
+    private String colour;
+
+    @Column(name = "sex", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Sex sex;
+
+    @Column(name = "birth_date")
+    private LocalDate birthday;
+
+    @Column(name = "microchip_id", unique = true, length = 20 )
+    private String microchip_id;
+
+    public enum Sex{
+        M,F
+    }
 }
